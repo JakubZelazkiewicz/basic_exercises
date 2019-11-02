032e6117b21888fed54445dd7e0e62bef7b153f5
kZelazkiewicz <kuba.totu.totam@gmail.com>
2019-11-02 23:06:04 +0300

    reflex game - nice version

--- js/reflex_game/timer.js
+++ js/reflex_game/timer.js
@@ -1,23 +1,43 @@
 var setTime = 0;
 var flag =true;
 var value = 0;
+var no1=0;
 
 function timer(){
-	if(flag == true && setTime<1000)
+	document.getElementById("select").setAttribute("value", "STOP");
+	document.getElementById("select").setAttribute("onclick", "timerBreak()");
+	
+	if(flag == true && setTime<1000){
 		setTime = setTime+1;
-    
-		document.getElementById("timeCounter").innerHTML = setTime;
-		
-		setTimeout("timer()", 10); 
-      }
+    	document.getElementById("timeCounter").innerHTML = setTime;
+		setTimeout("timer()", 1); 
+		}
+		else if ( setTime>=1400) {
+			alert("You missed the game");
+			timerBreak();				
+		}
+		else{
+		    var no1 = document.getElementById("target").textContent;
+			var no2 = document.getElementById("timeCounter").textContent;
+			alert(no1-no2);
+			}
+}
+	
 	  
+function findNumber(){
+	document.getElementById("select").setAttribute("value", "START");
+	document.getElementById("select").setAttribute("onclick", "timer()");
+	var value = Math.floor(Math.random()*1000);
+	document.getElementById("target").innerHTML = value;
+}
+
  function timerBreak(){
 flag = false;
 }
 
-function findNumber(){
-	var value = Math.floor(Math.random()*1000);
-	document.getElementById("target").innerHTML = value
+function reloadPage(){
+	location.reload();
 }
+
       
    
