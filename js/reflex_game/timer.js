4829108b96bf88ab525cd08254ffc8fb4f524a62
JakubZelazkiewicz <32820590+JakubZelazkiewicz@users.noreply.github.com>
2019-11-06 00:44:43 +0300

    game reflex - update time count mechanism

--- js/reflex_game/timer.js
+++ js/reflex_game/timer.js
@@ -1,49 +1,45 @@
 var setTime = 0;
-var flag =true;
+var flag = true;
 var value = 0;
-var no1=0;
+var no1 = 0;
+var interval;
 
-function timer(){
-	document.getElementById("select").setAttribute("value", "STOP");
-	document.getElementById("select").setAttribute("onclick", "timerBreak()");
-	
-	if(flag == true && setTime<1000){
-		setTime = setTime+1;
-    	document.getElementById("timeCounter").innerHTML = setTime;
-		setTimeout("timer()", 1); 
-		}
-		else if ( setTime>=1400) {
-			alert("You missed the game");
-			timerBreak();				
-		}
-		else{
-		    var no1 = document.getElementById("target").textContent;
-			var no2 = document.getElementById("timeCounter").textContent;
-			var different = Math.abs(no1-no2);
-			if (different<20){
-				alert("You are amazing!");
-			}
-			else{
-			alert("You missed by :" + different);
-			}
-}
-}
-	
-	  
-function findNumber(){
-	document.getElementById("select").setAttribute("value", "START");
-	document.getElementById("select").setAttribute("onclick", "timer()");
-	var value = Math.floor(Math.random()*1000);
-	document.getElementById("target").innerHTML = value;
+function setSpeed() {
+    interval = setInterval('timer();', 10);
 }
 
- function timerBreak(){
-flag = false;
+function timer() {
+    document.getElementById("select").setAttribute("value", "STOP");
+    document.getElementById("select").setAttribute("onclick", "timerBreak()");
+    setTime = setTime + 1;
+    document.getElementById("timeCounter").innerHTML = setTime;
+    if (setTime >= 1400) {
+        alert("You missed the game");
+        timerBreak();
+    }
 }
 
-function reloadPage(){
-	location.reload();
+function findNumber() {
+    document.getElementById("select").setAttribute("value", "START");
+    document.getElementById("select").setAttribute("onclick", "setSpeed()");
+
+    var value = Math.floor(Math.random() * 1000);
+    document.getElementById("target").innerHTML = value;
 }
 
-      
-   
+function timerBreak() {
+    clearInterval(interval);
+
+    var no1 = document.getElementById("target").textContent;
+    var no2 = document.getElementById("timeCounter").textContent;
+    var different = Math.abs(no1 - no2);
+    if (different <= 20) {
+        alert("You are amazing!");
+    } else {
+        alert("You missed by :" + different);
+    }
+}
+
+function reloadPage() {
+    location.reload();
+}
